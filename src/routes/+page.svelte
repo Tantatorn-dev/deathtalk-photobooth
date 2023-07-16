602b182816783dc626051448f7438f31e7bf3be9
Tantatorn <timefear007@gmail.com>
2023-07-16 18:00:33 +0700

    feat: draft intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,21 @@
 <svelte:window bind:innerWidth={screenWidth} />
 
 {#if screenWidth > 768}
-	Please use mobile device
+	<p>Please use mobile device</p>
 {:else}
-	<div class="text-center mt-4">
-		<Button text="คลิกที่นี่เพื่อเริ่มเล่น" />
+	<div class="flex flex-col gap-3 ml-8 mr-8 mt-8">
+		<p class="text-primary text-5xl">Death Talk</p>
+		<p class="text-4xl">ความตาย<br />และชีวิต</p>
+		<p class="text-2xl">สำรวจความตาย<br />ฝึกฝนการปล่อยวาง<br />วางแผนการส่งต่อสิ่งสำคัญ</p>
+		<div class="flex flex-col w-full justify-end h-64">
+			<Button text="คลิกที่นี่เพื่อเริ่มเล่น" />
+		</div>
 	</div>
-	<ImageUploader />
 {/if}
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.black);
+		color: theme(colors.white);
 	}
 </style>
