be2ea8112aaf023aaabf9a5e2f05fec67a9e7b46
Tantatorn <timefear007@gmail.com>
2023-07-24 22:25:41 +0700

    feat: add intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Button from '../components/Button.svelte';
+	import Button from '../components/common/Button.svelte';
 
 	let screenWidth = 0;
 </script>
@@ -9,11 +9,10 @@
 {#if screenWidth > 768}
 	<p>Please use mobile device</p>
 {:else}
-	<div class="flex flex-col gap-3 ml-8 mr-8 mt-8">
-		<p class="text-primary text-5xl">Death Talk</p>
-		<p class="text-4xl">ความตาย<br />และชีวิต</p>
-		<p class="text-2xl">สำรวจความตาย<br />ฝึกฝนการปล่อยวาง<br />วางแผนการส่งต่อสิ่งสำคัญ</p>
-		<div class="flex flex-col w-full justify-end h-64">
+	<div class="flex flex-col gap-3 ml-8 mr-8 mt-20">
+		<p class="text-primary text-4xl text-center">ยินดีต้อนรับสู่ <br/> Death Talk</p>
+		<p class="text-2xl text-center">คุณเตรียมพร้อมสำหรับ “ความตาย” มากน้อยขนาดไหน?</p>
+		<div class="flex flex-col w-full justify-end h-72">
 			<Button text="คลิกที่นี่เพื่อเริ่มเล่น" />
 		</div>
 	</div>
