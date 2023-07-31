fcbc2d7d8c218b918841b43ce151c700e625a837
Tantatorn <timefear007@gmail.com>
2023-08-01 06:47:14 +0700

    style: add IBM Plex Sans Thai Looped

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,19 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300;400;600;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <slot />
+
+<style>
+	:global(body) {
+		font-family: 'IBM Plex Sans Thai Looped', sans-serif;
+	}
+</style>
