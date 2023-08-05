830b2565c6bce93748d57525ad050bae769a5f29
Tantatorn <timefear007@gmail.com>
2023-08-05 15:14:13 +0700

    feat: add disclaimer text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col gap-3 mt-20 ml-8 mr-8">
 	<p class="text-4xl text-center">ยินดีต้อนรับสู่ <br /></p>
-	<SplashText className="text-4xl text-center">Death Talk</SplashText>
+	<SplashText className="text-4xl text-center p-1 pb-2">Death Talk</SplashText>
 	<p class="mt-8 font-light text-center">คุณเตรียมพร้อมสำหรับ <br> “ความตาย” มากน้อยขนาดไหน?</p>
 	<div class="flex flex-col justify-end h-72">
 		<div class="flex flex-row justify-center">
--- src/routes/disclaimer/+page.svelte
+++ src/routes/disclaimer/+page.svelte
@@ -3,15 +3,13 @@
 	import SplashText from '../../components/common/SplashText.svelte';
 
 	setTimeout(() => {
-		goto('/photo');
+		//goto('/photo');
 	}, 5000);
 </script>
 
-<div class="flex flex-col gap-3 mt-20 ml-8 mr-8">
+<div class="flex flex-col gap-3 mt-20 ml-4 mr-4">
 	<SplashText className="text-3xl text-center">คำเตือน</SplashText>
-	<p class="mt-4 text-sm text-center">
-		Lorem ipsum dolor sit amet consectetur. Hac pharetra quis in orci risus at suspendisse. Ornare
-		elementum erat sed pulvinar mattis sed. Maecenas placerat tempor magna varius natoque.
-		Sollicitudin varius tortor est massa sed enim.
+	<p class="mt-4 text-xs text-center">
+		เว็บไซต์นี้มีเนื้อหาเกี่ยวกับความตาย หากคุณรู้สึกไม่โอเค <br> “สามารถหยุดเล่นได้ตลอดเวลา”
 	</p>
 </div>
