37efa79b13a7b0755745cff17757997eaa054915
Tantatorn <timefear007@gmail.com>
2023-08-10 07:20:59 +0700

    feat: add intermission page

---
+++ src/routes/explore/intermission/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import VineFrame from '../../../components/frame/VineFrame.svelte';
+
+	setInterval(() => {
+		goto('/explore/5');
+	}, 5000);
+</script>
+
+<VineFrame>
+	<div class="flex flex-col items-center justify-center h-full pl-8 pr-8">
+		<p class="text-xl">ยังไหวหรือเปล่า? <br /> หมวดสุดท้ายแล้ว..</p>
+	</div>
+</VineFrame>
