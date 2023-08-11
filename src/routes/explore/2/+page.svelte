2e1680b918c9a2e93032b90164c51924d8591242
Tantatorn <timefear007@gmail.com>
2023-08-11 21:56:18 +0700

    feat: add rose img

--- src/routes/explore/2/+page.svelte
+++ src/routes/explore/2/+page.svelte
@@ -6,29 +6,34 @@
 	let index = 0;
 </script>
 
-<div class="flex flex-col gap-3 mt-20 ml-8 mr-8">
-	<Header
-		chapter={2}
-		title="การช่วยเหลือผู้ป่วย"
-		subtitle="ความสามารถของคุณหากอยู่ในบทบาทที่ต้องช่วยเหลือผู้ป่วย"
-	/>
-	<div class="flex flex-col gap-10 mt-10">
-		{#if index == 0}
-			<Question question="1. คุณสามารถหาอาหารหรือป้อนอาหารให้พวกเขาได้ไหม?" />
-			<Question question="2. คุณสามารถอาบน้ำให้เขาได้ไหม?" />
-			<div class="flex justify-end">
-				<NextButton
-					onClick={() => {
-						index = 1;
-					}}
-				/>
-			</div>
-		{:else if index == 1}
-			<Question question="3. คุณสามารถยกหรือเคลื่อนย้ายพวกเขาได้ไหม?" />
-			<Question question="4. คุณสามารถฉีดยาให้พวกเขาได้ไหม?" />
-			<div class="flex justify-end">
-				<NextButton target="/explore/3" />
-			</div>
-		{/if}
+<div class="relative w-full h-full">
+	<div class="flex flex-col gap-3 mt-20 ml-8 mr-8">
+		<Header
+			chapter={2}
+			title="การช่วยเหลือผู้ป่วย"
+			subtitle="ความสามารถของคุณหากอยู่ในบทบาทที่ต้องช่วยเหลือผู้ป่วย"
+		/>
+		<div class="flex flex-col gap-10 mt-10">
+			{#if index == 0}
+				<Question question="1. คุณสามารถหาอาหารหรือป้อนอาหารให้พวกเขาได้ไหม?" />
+				<Question question="2. คุณสามารถอาบน้ำให้เขาได้ไหม?" />
+				<div class="flex justify-end">
+					<NextButton
+						onClick={() => {
+							index = 1;
+						}}
+					/>
+				</div>
+			{:else if index == 1}
+				<Question question="3. คุณสามารถยกหรือเคลื่อนย้ายพวกเขาได้ไหม?" />
+				<Question question="4. คุณสามารถฉีดยาให้พวกเขาได้ไหม?" />
+				<div class="flex justify-end">
+					<NextButton target="/explore/3" />
+				</div>
+			{/if}
+		</div>
+	</div>
+	<div class="absolute w-full bottom-20">
+		<img src="/misc/rose.svg" class="ml-auto mr-auto" alt="rose" />
 	</div>
 </div>
