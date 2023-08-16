f2de9e76c3d2ac94a48e9ec009fd391ef0cf3a7a
Tantatorn <timefear007@gmail.com>
2023-08-16 08:01:35 +0700

    feat: init result page

---
+++ src/routes/result/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import SplashText from '../../components/common/SplashText.svelte';
+</script>
+
+<div class="flex flex-col gap-3 pt-10 ml-8 mr-8">
+	<SplashText className="pt-4 pb-4 pl-8 pr-8 text-center text-xl">ผลลัพธ์และของที่ระลึก</SplashText>
+	<p class="mt-1 font-bold text-center">
+		คุณมีการรู้เท่าทันความตายได้ระดับ 3
+	</p>
+	<p class="text-xs text-center">
+		คุณถือว่ามีความรู้ด้านการรู้เท่าทันความตายในระดับ
+		ค่อนข้างน้อย
+	</p>
+</div>
