12ac4dd8928077e7f299e6f634279917d3b1c6b1
Tantatorn <timefear007@gmail.com>
2023-08-16 23:13:21 +0700

    feat: init FrameSelector

---
+++ src/components/result/FrameSelector.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Frame from "../common/Frame.svelte";
+
+</script>
+
+<div class="flex flex-row items-center justify-between w-full">
+	<img src="/arrow/arrow_l.svg" class="w-8 h-8" alt="arrow left" />
+	<Frame src="/placeholder.svg" />
+	<img src="/arrow/arrow_r.svg" class="w-8 h-8" alt="arrow right" />
+</div>
--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -1,14 +1,16 @@
 <script>
 	import SplashText from '../../components/common/SplashText.svelte';
+	import FrameSelector from '../../components/result/FrameSelector.svelte';
 </script>
 
-<div class="flex flex-col gap-3 pt-10 ml-8 mr-8">
+<div class="flex flex-col gap-3 pt-10 ml-4 mr-4">
 	<SplashText className="pt-4 pb-4 pl-8 pr-8 text-center text-xl">ผลลัพธ์และของที่ระลึก</SplashText>
 	<p class="mt-1 font-bold text-center">
 		คุณมีการรู้เท่าทันความตายได้ระดับ 3
 	</p>
-	<p class="text-xs text-center">
+	<p class="pl-6 pr-6 text-xs text-center">
 		คุณถือว่ามีความรู้ด้านการรู้เท่าทันความตายในระดับ
 		ค่อนข้างน้อย
 	</p>
+	<FrameSelector />
 </div>
