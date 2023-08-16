bd35a19f53a2a868e7ba7ec60877cfc59bb99d0c
Tantatorn <timefear007@gmail.com>
2023-08-16 23:46:19 +0700

    feat: add subtitle text

--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -5,12 +5,10 @@
 
 <div class="flex flex-col gap-3 pt-10">
 	<SplashText className="pt-4 pb-4 pl-8 pr-8 text-center text-xl">ผลลัพธ์และของที่ระลึก</SplashText>
-	<p class="mt-1 font-bold text-center">
-		คุณมีการรู้เท่าทันความตายได้ระดับ 3
-	</p>
+	<p class="mt-1 font-bold text-center">คุณมีการรู้เท่าทันความตายได้ระดับ 3</p>
 	<p class="pl-6 pr-6 text-xs text-center">
-		คุณถือว่ามีความรู้ด้านการรู้เท่าทันความตายในระดับ
-		ค่อนข้างน้อย
+		คุณถือว่ามีความรู้ด้านการรู้เท่าทันความตายในระดับ ค่อนข้างน้อย
 	</p>
 	<FrameSelector />
+	<p class="text-sm text-center">ร่วมแบ่งปันเรื่องราวของคุณผ่าน <br> #deathtalkth</p>
 </div>
