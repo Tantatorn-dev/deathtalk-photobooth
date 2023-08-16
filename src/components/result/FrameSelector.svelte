f6a0e5ae294440e58ebb03803e7ba3aca376dd4c
Tantatorn <timefear007@gmail.com>
2023-08-16 23:42:11 +0700

    feat: add background to frame selector

--- src/components/result/FrameSelector.svelte
+++ src/components/result/FrameSelector.svelte
@@ -1,10 +1,14 @@
 <script>
-	import Frame from "../common/Frame.svelte";
-
+	import Frame from '../common/Frame.svelte';
 </script>
 
-<div class="flex flex-row items-center justify-between w-full bg-black">
+<div
+	class="flex flex-row items-center justify-between w-full bg-[url(/misc/bg_rose.png)] pt-4 pb-4"
+>
 	<img src="/arrow/arrow_l.svg" class="w-8 h-8" alt="arrow left" />
 	<Frame src="/placeholder.svg" />
 	<img src="/arrow/arrow_r.svg" class="w-8 h-8" alt="arrow right" />
+</div>
+<div class="ml-10 mr-10 -mt-5">
+	<button class="button-primary">บันทึกรูปภาพ</button>
 </div>
