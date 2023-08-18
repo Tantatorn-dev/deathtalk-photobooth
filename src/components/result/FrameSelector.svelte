6d25a076849055710b189bd7fb66c15522261536
Tantatorn <timefear007@gmail.com>
2023-08-18 08:04:27 +0700

    feat: add svelte-carousel

--- src/components/result/FrameSelector.svelte
+++ src/components/result/FrameSelector.svelte
@@ -1,14 +1,32 @@
 <script>
 	import Frame from '../common/Frame.svelte';
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
 </script>
 
 <div
 	class="flex flex-row items-center justify-between w-full bg-[url(/misc/bg_rose.png)] pt-4 pb-4"
 >
-	<img src="/arrow/arrow_l.svg" class="w-8 h-8" alt="arrow left" />
-	<Frame src="/placeholder.svg" />
-	<img src="/arrow/arrow_r.svg" class="w-8 h-8" alt="arrow right" />
+	{#if browser}
+		<Carousel>
+			<div class="wrapper-photo">
+				<Frame src="/placeholder.svg" />
+			</div>
+			<div class="wrapper-photo">
+				<Frame src="/placeholder.svg" />
+			</div>
+			<div class="wrapper-photo">
+				<Frame src="/placeholder.svg" />
+			</div>
+		</Carousel>
+	{/if}
 </div>
 <div class="ml-10 mr-10 -mt-5">
 	<button class="button-primary">บันทึกรูปภาพ</button>
 </div>
+
+<style lang="postcss">
+	.wrapper-photo {
+		@apply flex flex-row items-center justify-center;
+	}
+</style>
