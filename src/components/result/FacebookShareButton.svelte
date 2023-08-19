61d632df56196fb991a52f56f8da31f63c50d25e
Tantatorn <timefear007@gmail.com>
2023-08-19 13:42:26 +0700

    feat: add share link to facebook

--- src/components/result/FacebookShareButton.svelte
+++ src/components/result/FacebookShareButton.svelte
@@ -1,4 +1,15 @@
-<button class="flex flex-row pt-2 pb-2 pl-5 pr-5 text-white bg-blue-500 gap-x-2">
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<button
+	on:click={() => {
+		goto(
+			'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdeathtalk-photobooth.vercel.app%2F&amp;src=sdkpreparse'
+		);
+	}}
+	class="flex flex-row pt-2 pb-2 pl-5 pr-5 text-white bg-blue-500 gap-x-2"
+>
 	<img src="/misc/facebook.svg" class="w-6 h-6" alt="facebook" />
 	<span>แชร์เว็บนี้ให้เพื่อนคุณเล่น</span>
 </button>
