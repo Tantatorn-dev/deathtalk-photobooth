986451a390bcfaeeb9ed793bf883ddd0ac1646be
Tantatorn <timefear007@gmail.com>
2023-08-19 12:48:28 +0700

    feat: add img exporter

--- src/components/result/FrameSelector.svelte
+++ src/components/result/FrameSelector.svelte
@@ -1,7 +1,71 @@
-<script>
+<script lang="ts">
 	import Frame from '../common/Frame.svelte';
 	import Carousel from 'svelte-carousel';
 	import { browser } from '$app/environment';
+	import { avatar, birthDate, name } from '../../store';
+	import { toThaiDate } from '../../utils/utils';
+
+	let avatarValue: string;
+
+	avatar.subscribe((value) => {
+		if (value) {
+			avatarValue = value;
+		} else {
+			avatarValue = '/placeholder.svg';
+		}
+	});
+
+	let nameValue: string;
+	name.subscribe((value) => {
+		nameValue = value;
+	});
+
+	let birthDateValue: Date;
+	birthDate.subscribe((value) => {
+		if (value) birthDateValue = new Date(value);
+		else birthDateValue = new Date();
+	});
+
+	const onDownload = () => {
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+
+		canvas.width = 244;
+		canvas.height = 332;
+
+		const frame = new Image();
+		frame.src = '/frame.png';
+
+		const img = new Image();
+		img.src = avatarValue;
+
+		if (ctx) {
+			// draw bg
+			ctx.fillStyle = '#000';
+			ctx.rect(0, 0, canvas.width, canvas.height);
+			ctx.fill();
+
+			// draw frame bg
+			ctx.fillStyle = '#fff';
+			ctx.fillRect(40, 40, 180, 250);
+
+			ctx.drawImage(frame, 0, 0);
+			ctx.drawImage(img, 50, 50, 144, 144);
+
+			ctx.fillStyle = '#000';
+			ctx.textAlign = 'center';
+			ctx.font = "11.5px IBM Plex Sans Thai Looped";
+
+			ctx.fillText(nameValue, 120, 210);
+			ctx.fillText(`ชาตะ ${toThaiDate(birthDateValue)}`, 120, 230);
+			ctx.fillText(`มรณะ ${toThaiDate(new Date())}`, 120, 250);
+
+			const link = document.createElement('a');
+			link.download = 'deathtalkth.png';
+			link.href = canvas.toDataURL('image/png');
+			link.click();
+		}
+	};
 </script>
 
 <div
@@ -10,19 +74,19 @@
 	{#if browser}
 		<Carousel>
 			<div class="wrapper-photo">
-				<Frame src="/placeholder.svg" />
+				<Frame src={avatarValue} />
 			</div>
 			<div class="wrapper-photo">
-				<Frame src="/placeholder.svg" />
+				<Frame src={avatarValue} />
 			</div>
 			<div class="wrapper-photo">
-				<Frame src="/placeholder.svg" />
+				<Frame src={avatarValue} />
 			</div>
 		</Carousel>
 	{/if}
 </div>
 <div class="ml-10 mr-10 -mt-5">
-	<button class="button-primary">บันทึกรูปภาพ</button>
+	<button on:click={onDownload} class="button-primary">บันทึกรูปภาพ</button>
 </div>
 
 <style lang="postcss">
