f5e03f6bef24f8ebac625a0485cd13ec09b0b1e1
Tantatorn <timefear007@gmail.com>
2023-08-20 14:23:48 +0700

    feat: add new splash text

--- src/routes/prepare/+page.svelte
+++ src/routes/prepare/+page.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
-	import SplashText from '../../components/common/SplashText.svelte';
+	import { goto } from '$app/navigation';
+	import SplashText2 from '../../components/common/SplashText2.svelte';
 
-	let index = 1;
+	let index = 0;
+	setInterval(() => {
+		if(index == 2) {
+			goto('/explore');
+		}
+
+		index = (index + 1) % 3;
+	}, 3000)
 </script>
 
 <div class="flex flex-col items-center justify-between w-full h-full">
 	<p class="mt-20 text-xl">วิธีเตรียมตัวตาย {index + 1}/3</p>
 	<div class="flex flex-col items-center">
 		{#if index == 0}
-			<SplashText className="text-xl text-center pl-10 pr-10 pb-2">พินัยกรรม</SplashText>
-			<p class="text-center">
+		 	<SplashText2 text="พินัยกรรม" />
+			<p class="text-description">
 				ลายลักษณ์อักษรที่เขียนคำสั่งสุดท้ายเพื่อแสดงเจตนา
 				คนที่ยังอยู่หมดปัญหาเรื่องการแบ่งทรัพย์สินมรดก สมบัติของผู้ตาย
 			</p>
 		{:else if index == 1}
-			<SplashText className="text-xl text-center pl-10 pr-10 pb-2">บริจาคอวัยวะ/ร่างกาย</SplashText
-			>
-			<p class="text-center">
+		 	<SplashText2 text="บริจาคอวัยวะ/ร่างกาย" />
+			<p class="text-description">
 				บริจาคอวัยวะหรือร่างการ ให้กับผู้ที่อวัยวะเสื่อมสภาพ หรือใช้ในการศึกษา
 				<br />
 				<br />
 				เติมเต็มความต้องการภายใน ในการทอบอวัยวะหรือร่างกายให้กับคนอื่น
 			</p>
 		{:else if index == 2}
-			<SplashText className="text-xl text-center pl-10 pr-10 pb-2"
-				>สมุดเบาใจ living will</SplashText
-			>
-			<p class="text-center">
+		 	<SplashText2 text="สมุดเบาใจ living will" />
+			<p class="text-description">
 				สมุดแสดงความต้องการสุดท้าย เกี่ยวกับสุขภาพและการตายดี
 				<br />
 				<br />
@@ -42,3 +47,9 @@
 		<img src="/misc/note_2.svg" alt="baojai note" />
 	{/if}
 </div>
+
+<style lang="postcss">
+	.text-description {
+		@apply text-center pl-10 pr-10;
+	}
+</style>
