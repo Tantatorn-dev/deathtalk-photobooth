2d1b854d8c27702eb34c5f7614140c7621d752d8
Tantatorn <timefear007@gmail.com>
2023-08-28 20:47:25 +0700

    feat: add fade wrapper

---
+++ src/components/common/FadeWrapper.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let isShow = false;
+
+	setTimeout(() => {
+		isShow = true;
+	}, 1000);
+</script>
+
+{#if isShow}
+	<div in:fade out:fade>
+		<slot />
+	</div>
+{/if}
--- src/routes/disclaimer/+page.svelte
+++ src/routes/disclaimer/+page.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
+	import FadeWrapper from '../../components/common/FadeWrapper.svelte';
 	import NextButton from '../../components/common/NextButton.svelte';
 	import SplashText from '../../components/common/SplashText.svelte';
 </script>
 
 <div class="relative w-full h-full">
-	<div class="flex flex-col gap-3 ml-4 mr-4">
-		<img src="/deathtalk.svg" alt="family" class="mt-5 mb-5 ml-auto mr-auto" />
-		<SplashText className="text-3xl text-center">คำเตือน</SplashText>
-		<p class="mt-4 text-xs text-center">
-			เว็บไซต์นี้มีเนื้อหาเกี่ยวกับความตาย หากคุณรู้สึกไม่โอเค <br /> “สามารถหยุดเล่นได้ตลอดเวลา”
-		</p>
-		<div class="flex flex-row justify-center mt-20">
-			<NextButton target="/photo" />
+	<FadeWrapper>
+		<div class="flex flex-col gap-3 ml-4 mr-4">
+			<img src="/deathtalk.svg" alt="family" class="mt-5 mb-5 ml-auto mr-auto" />
+			<SplashText className="text-3xl text-center">คำเตือน</SplashText>
+			<p class="mt-4 text-xs text-center">
+				เว็บไซต์นี้มีเนื้อหาเกี่ยวกับความตาย หากคุณรู้สึกไม่โอเค <br /> “สามารถหยุดเล่นได้ตลอดเวลา”
+			</p>
+			<div class="flex flex-row justify-center mt-20">
+				<NextButton target="/photo" />
+			</div>
 		</div>
-	</div>
+	</FadeWrapper>
 	<img src="/misc/sun.svg" alt="family" class="absolute bottom-48 right-20" />
 	<img src="/misc/mountain.svg" alt="family" class="absolute bottom-0 right-0" />
 </div>
