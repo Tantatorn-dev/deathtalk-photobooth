bb583896c9c9f9d96bc227f68b7b85840ccd88f7
Tantatorn <timefear007@gmail.com>
2023-08-28 20:12:22 +0700

    feat: add next button to prepare to die

--- src/routes/prepare/+page.svelte
+++ src/routes/prepare/+page.svelte
@@ -1,50 +1,57 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import NextButton from '../../components/common/NextButton.svelte';
 	import SplashText2 from '../../components/common/SplashText2.svelte';
 
 	let index = 0;
-	setTimeout(() => {
-		index = 0;
-	}, 5000)
-
-	setTimeout(() => {
-		index = 1;
-	}, 10000)
-
-	setTimeout(() => {
-		index = 2;
-	}, 15000)
-
-	setTimeout(() => {
-		goto('/explore');
-	}, 20000)
 </script>
 
 <div class="flex flex-col items-center justify-between w-full h-full">
 	<p class="mt-20 text-xl">วิธีเตรียมตัวตาย {index + 1}/3</p>
 	<div class="flex flex-col items-center">
 		{#if index == 0}
-		 	<SplashText2 text="พินัยกรรม" />
+			<SplashText2 text="พินัยกรรม" />
 			<p class="text-description">
 				ลายลักษณ์อักษรที่เขียนคำสั่งสุดท้ายเพื่อแสดงเจตนา
 				คนที่ยังอยู่หมดปัญหาเรื่องการแบ่งทรัพย์สินมรดก สมบัติของผู้ตาย
 			</p>
+			<div class="next-btn-container">
+				<NextButton
+					onClick={() => {
+						index++;
+					}}
+				/>
+			</div>
 		{:else if index == 1}
-		 	<SplashText2 text="บริจาคอวัยวะ/ร่างกาย" />
+			<SplashText2 text="บริจาคอวัยวะ/ร่างกาย" />
 			<p class="text-description">
 				บริจาคอวัยวะหรือร่างการ ให้กับผู้ที่อวัยวะเสื่อมสภาพ หรือใช้ในการศึกษา
 				<br />
 				<br />
 				เติมเต็มความต้องการภายใน ในการทอบอวัยวะหรือร่างกายให้กับคนอื่น
 			</p>
+			<div class="next-btn-container">
+				<NextButton
+					onClick={() => {
+						index++;
+					}}
+				/>
+			</div>
 		{:else if index == 2}
-		 	<SplashText2 text="สมุดเบาใจ living will" />
+			<SplashText2 text="สมุดเบาใจ living will" />
 			<p class="text-description">
 				สมุดแสดงความต้องการสุดท้าย เกี่ยวกับสุขภาพและการตายดี
 				<br />
 				<br />
 				เบาใจตัวเองและคนใกล้ชิด เพราะมีแนวทางปฏิบัติที่ชัดเจน
 			</p>
+			<div class="next-btn-container">
+				<NextButton
+					onClick={() => {
+						goto('/explore');
+					}}
+				/>
+			</div>
 		{/if}
 	</div>
 	{#if index == 0}
@@ -59,5 +66,9 @@
 <style lang="postcss">
 	.text-description {
 		@apply text-center pl-10 pr-10;
+	}
+
+	.next-btn-container {
+		@apply mt-10;
 	}
 </style>
