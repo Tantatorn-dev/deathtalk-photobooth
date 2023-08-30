fa57ee521b39869191672625254f1ba35175181e
Tantatorn <timefear007@gmail.com>
2023-08-30 20:24:35 +0700

    feat: add fade wrapper to photo page

--- src/routes/photo/+page.svelte
+++ src/routes/photo/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import FadeWrapper from '../../components/common/FadeWrapper.svelte';
 	import NextButton from '../../components/common/NextButton.svelte';
 	import { avatar, birthDate, name } from '../../store';
 
@@ -49,42 +50,44 @@
 	};
 </script>
 
-<div class="flex flex-col items-center gap-2 pt-16 pl-16 pr-16">
-	{#if avatarValue}
-		<img src={avatarValue} class="w-48 h-48" alt="avatar" />
-	{:else}
-		<img src="/placeholder.svg" class="w-48 h-48" alt="placeholder" />
-	{/if}
+<FadeWrapper>
+	<div class="flex flex-col items-center gap-2 pt-16 pl-16 pr-16">
+		{#if avatarValue}
+			<img src={avatarValue} class="w-48 h-48" alt="avatar" />
+		{:else}
+			<img src="/placeholder.svg" class="w-48 h-48" alt="placeholder" />
+		{/if}
 
-	<div class="flex flex-col w-full gap-1 mt-4">
-		<p class="text-sm">ชื่อ*</p>
-		<input
-			type="text"
-			on:input={onUpdateName}
-			class="button-secondary bg-secondary"
-			placeholder="พิมพ์ชื่อ"
-		/>
-	</div>
+		<div class="flex flex-col w-full gap-1 mt-4">
+			<p class="text-sm">ชื่อ*</p>
+			<input
+				type="text"
+				on:input={onUpdateName}
+				class="button-secondary bg-secondary"
+				placeholder="พิมพ์ชื่อ"
+			/>
+		</div>
 
-	<div class="flex flex-col w-full gap-1 mt-4">
-		<p class="text-sm">วันเกิด*</p>
-		<input
-			type="date"
-			on:input={onUpdateBirthDate}
-			class="button-secondary bg-secondary"
-			placeholder="เลือกวันเกิด"
-		/>
-	</div>
+		<div class="flex flex-col w-full gap-1 mt-4">
+			<p class="text-sm">วันเกิด*</p>
+			<input
+				type="date"
+				on:input={onUpdateBirthDate}
+				class="button-secondary bg-secondary"
+				placeholder="เลือกวันเกิด"
+			/>
+		</div>
 
-	<div class="flex flex-col w-full gap-1 mt-4">
-		<p class="text-sm">รูปโปรไฟล์</p>
-		<button class="button-secondary" on:click={onOpenFileInput}>อัปโหลดรูปภาพ</button>
-	</div>
+		<div class="flex flex-col w-full gap-1 mt-4">
+			<p class="text-sm">รูปโปรไฟล์</p>
+			<button class="button-secondary" on:click={onOpenFileInput}>อัปโหลดรูปภาพ</button>
+		</div>
 
-	<div class="flex flex-row justify-end w-full mt-8">
-		<NextButton target="/intro" disabled={!nameValue || !birthDateValue} />
+		<div class="flex flex-row justify-end w-full mt-8">
+			<NextButton target="/intro" disabled={!nameValue || !birthDateValue} />
+		</div>
 	</div>
-</div>
+</FadeWrapper>
 <input
 	class="hidden"
 	type="file"
