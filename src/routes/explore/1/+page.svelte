b79cb08eb9d606d58fb1fcdcc61abe11342868bc
Tantatorn <timefear007@gmail.com>
2023-09-02 12:53:12 +0700

    feat: add fade in/out to explore 1,2

--- src/routes/explore/1/+page.svelte
+++ src/routes/explore/1/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { fade } from 'svelte/transition';
+	import FadeWrapper from '../../../components/common/FadeWrapper/FadeWrapper.svelte';
 	import NextButton from '../../../components/common/NextButton.svelte';
 	import Header from '../../../components/explore/Header.svelte';
 	import Question from '../../../components/explore/Question.svelte';
@@ -11,6 +14,28 @@
 	answers.subscribe((value) => {
 		answersValue = value;
 	});
+
+	let isShow1 = false;
+
+	$: if (index == 0) {
+		setTimeout(() => {
+			isShow1 = true;
+		}, 1000);
+	}
+
+	let isShow2 = false;
+
+	$: if (index == 1) {
+		setTimeout(() => {
+			isShow2 = true;
+		}, 1000);
+	}
+
+	let isShowImg = false;
+
+	setTimeout(() => {
+		isShowImg = true;
+	}, 1000);
 </script>
 
 <div class="relative w-full h-full">
@@ -24,27 +49,43 @@
 		</div>
 		<div class="flex flex-col gap-10 mt-10">
 			{#if index == 0}
-				<Question question="1. คุณกล้าพูดเรื่องความตายกับเพื่อนสนิทมากแค่ไหน?" name="a1" />
-				<Question question="2. คุณกล้าพูดเรื่องความตายกับเด็กมากแค่ไหน?" name="a2" />
-				<div class="flex justify-end">
-					<NextButton
-						disabled={!isAllAnswered(answersValue, ['a1', 'a2'])}
-						onClick={() => {
-							index = 1;
-						}}
-					/>
-				</div>
+				<FadeWrapper className="flex flex-col gap-3" isShow={isShow1}>
+					<Question question="1. คุณกล้าพูดเรื่องความตายกับเพื่อนสนิทมากแค่ไหน?" name="a1" />
+					<Question question="2. คุณกล้าพูดเรื่องความตายกับเด็กมากแค่ไหน?" name="a2" />
+					<div class="flex justify-end">
+						<NextButton
+							disabled={!isAllAnswered(answersValue, ['a1', 'a2'])}
+							onClick={() => {
+								isShow1 = false;
+
+								setTimeout(() => {
+									index = 1;
+								}, 1000);
+							}}
+						/>
+					</div>
+				</FadeWrapper>
 			{:else if index == 1}
-				<Question question="3. คุณพูดเรื่องความตายกับเพื่อนสนิท?" name="a3" />
-				<Question question="4. คุณพูดเรื่องความตายกับเด็ก?" name="a4" />
-				<div class="flex justify-end">
-					<NextButton
-						disabled={!isAllAnswered(answersValue, ['a3', 'a4'])}
-						target="/explore/2"
-					/>
-				</div>
+				<FadeWrapper className="flex flex-col gap-3" isShow={isShow2}>
+					<Question question="3. คุณพูดเรื่องความตายกับเพื่อนสนิท?" name="a3" />
+					<Question question="4. คุณพูดเรื่องความตายกับเด็ก?" name="a4" />
+					<div class="flex justify-end">
+						<NextButton
+							disabled={!isAllAnswered(answersValue, ['a3', 'a4'])}
+							onClick={() => {
+								isShow2 = false;
+
+								setTimeout(() => {
+									goto('/explore/2');
+								}, 1000);
+							}}
+						/>
+					</div>
+				</FadeWrapper>
 			{/if}
 		</div>
 	</div>
-	<img src="/misc/family.svg" alt="family" class="absolute bottom-0 z-10 w-full" />
+	{#if isShowImg}
+		<img src="/misc/family.svg" alt="family" class="absolute bottom-0 z-10 w-full" in:fade />
+	{/if}
 </div>
--- src/routes/explore/2/+page.svelte
+++ src/routes/explore/2/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { fade } from 'svelte/transition';
+	import FadeWrapper from '../../../components/common/FadeWrapper/FadeWrapper.svelte';
 	import NextButton from '../../../components/common/NextButton.svelte';
 	import Header from '../../../components/explore/Header.svelte';
 	import Question from '../../../components/explore/Question.svelte';
@@ -11,6 +14,28 @@
 	answers.subscribe((value) => {
 		answersValue = value;
 	});
+
+	let isShow1 = false;
+
+	$: if (index == 0) {
+		setTimeout(() => {
+			isShow1 = true;
+		}, 1000);
+	}
+
+	let isShow2 = false;
+
+	$: if (index == 1) {
+		setTimeout(() => {
+			isShow2 = true;
+		}, 1000);
+	}
+
+	let isShowImg = false;
+
+	setTimeout(() => {
+		isShowImg = true;
+	}, 1000);
 </script>
 
 <div class="relative w-full h-full">
@@ -24,26 +49,45 @@
 		</div>
 		<div class="flex flex-col gap-10 mt-10">
 			{#if index == 0}
-				<Question question="1. คุณสามารถหาอาหารหรือป้อนอาหารให้พวกเขาได้ไหม?" name="b1" />
-				<Question question="2. คุณสามารถอาบน้ำให้เขาได้ไหม?" name="b2" />
-				<div class="flex justify-end">
-					<NextButton
-						disabled={!isAllAnswered(answersValue, ['b1', 'b2'])}
-						onClick={() => {
-							index = 1;
-						}}
-					/>
-				</div>
+				<FadeWrapper className="flex flex-col gap-3" isShow={isShow1}>
+					<Question question="1. คุณสามารถหาอาหารหรือป้อนอาหารให้พวกเขาได้ไหม?" name="b1" />
+					<Question question="2. คุณสามารถอาบน้ำให้เขาได้ไหม?" name="b2" />
+					<div class="flex justify-end">
+						<NextButton
+							disabled={!isAllAnswered(answersValue, ['b1', 'b2'])}
+							onClick={() => {
+								isShow1 = false;
+
+								setTimeout(() => {
+									index = 1;
+								}, 1000);
+							}}
+						/>
+					</div>
+				</FadeWrapper>
 			{:else if index == 1}
-				<Question question="3. คุณสามารถยกหรือเคลื่อนย้ายพวกเขาได้ไหม?" name="b3" />
-				<Question question="4. คุณสามารถฉีดยาให้พวกเขาได้ไหม?" name="b4" />
-				<div class="flex justify-end">
-					<NextButton disabled={!isAllAnswered(answersValue, ['b3', 'b4'])} target="/explore/3" />
-				</div>
+				<FadeWrapper className="flex flex-col gap-3" isShow={isShow2}>
+					<Question question="3. คุณสามารถยกหรือเคลื่อนย้ายพวกเขาได้ไหม?" name="b3" />
+					<Question question="4. คุณสามารถฉีดยาให้พวกเขาได้ไหม?" name="b4" />
+					<div class="flex justify-end">
+						<NextButton
+							disabled={!isAllAnswered(answersValue, ['b3', 'b4'])}
+							onClick={() => {
+								isShow2 = false;
+
+								setTimeout(() => {
+									goto('/explore/3');
+								}, 1000);
+							}}
+						/>
+					</div>
+				</FadeWrapper>
 			{/if}
 		</div>
 	</div>
 	<div class="absolute bottom-0 z-10 w-full">
-		<img src="/misc/rose.svg" class="ml-auto mr-auto" alt="rose" />
+		{#if isShowImg}
+			<img src="/misc/rose.svg" class="ml-auto mr-auto" alt="rose" in:fade />
+		{/if}
 	</div>
 </div>
