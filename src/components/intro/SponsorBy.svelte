8f76bb7e2d1356629690d24ff318458fe1437ce9
Tantatorn <timefear007@gmail.com>
2023-11-22 21:56:40 +0700

    feat: increase size of sponsor logo

--- src/components/intro/SponsorBy.svelte
+++ src/components/intro/SponsorBy.svelte
@@ -1,5 +1,5 @@
 <div class="flex flex-row gap-x-2">
 	<p>สนับสนุนโดย</p>
-	<img src="/logo/sysi.png" alt="sysi" class="w-[28px] h-[28px]" />
-	<img src="/logo/thai_health.png" alt="rose" />
+	<img src="/logo/sysi.png" alt="sysi" class="w-[32px] h-[32px]" />
+	<img src="/logo/thai_health.png" alt="rose" class="w-[48px] h-[32px]"  />
 </div>
