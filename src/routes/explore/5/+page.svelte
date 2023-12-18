76dca6d9af564bea0dd96bba51cb2d7da03fc83d
Tantatorn <timefear007@gmail.com>
2023-12-18 19:53:11 +0700

    fix: questions typo

--- src/routes/explore/5/+page.svelte
+++ src/routes/explore/5/+page.svelte
@@ -27,13 +27,13 @@
 
 <FadeWrapper {isShow} className="flex flex-col gap-3 ml-8 mr-8">
 	<img src="/vine/vine_1.svg" class="w-24 -mb-6 -ml-10" alt="vine" />
-	<Header chapter={5} title="การเข้าถึงความช่วยเหลือ" />
+	<Header chapter={5} title="การเข้าถึงความช่วยเหลือ" subtitle="ถ้าคุณต้องดูแลผู้ป่วยระยะสุดท้าย คุณรู้ว่าจะเข้าถึงผู้คนต่อไปนี้ได้อย่างไร" />
 	<div class="flex flex-col gap-5 mt-10">
-		<Question question="1. คุณรู้จักคนที่สามารถช่วยฉันเข้าถึงการสนับสนุนในชุมชน?" name="e1" />
-		<Question question="2. คุณรู้จักคนที่ให้การดูแลผู้ป่วยที่กำลังจะตายในแต่ละวัน?" name="e2" />
-		<Question question="3. คุณสามารถเข้าถึงอุปกรณ์ที่จำเป็นสำหรับการดูแล?" name="e3" />
-		<Question question="4. คุณสามารถเข้าถึงการสนับสนุนที่เหมาะสมทางวัฒนธรรม?" name="e4" />
-		<Question question="5. คุณสามารถเข้าถึงการสนับสนุนทางอารมณ์สำหรับตัวเอง?" name="e5" />
+		<Question question="1. คุณสามารถเข้าถึงองค์กรที่ช่วยเหลือด้านการดูแลผู่ป่วยระยะสุดท้าย" name="e1" />
+		<Question question="2. คุณรู้จักองค์กรที่รับดูแลผู้ป่วยระยะสุดท้าย" name="e2" />
+		<Question question="3. คุณสามารถเข้าถึงอุปกรณ์ที่จำเป็นต้องใช้ในการดูแลผู้ป่วยระยะสุดท้าย" name="e3" />
+		<Question question="4. คุณสามารถเข้าถึงการดูแลผู้ป่วยระยะสุดท้ายในรูปแบบที่เหมาะสมตามวัฒนธรรมของคุณ" name="e4" />
+		<Question question="5. คุณสามารถเข้าถึงองค์กรที่ดูแลอารมณ์และจิตใจของคุณ จากการที่ต้องดูแลผู้ป่วยระยะสุดท้าย" name="e5" />
 		<div class="flex justify-between">
 			<BackButton onClick={() => goto(`/explore/4`)} />
 			<NextButton
